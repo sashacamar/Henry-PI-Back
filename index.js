430a6810d55caa16f7f446fcb2e4ff3ba154982e
Sasha Camargo <69942870+sashacamar@users.noreply.github.com>
2023-09-06 16:42:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -24,8 +24,7 @@ const port = process.env.PORT
 
 // Syncing all the models at once.
 conn.sync({ alter: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(port, () => {
     console.log(`Server raised in port ${port}`);
   });
 });
